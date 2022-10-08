e97bb3bae4ee37e5c14e6f3057514d098408ed05
Shuvo MH <96376185+shuvomh2000@users.noreply.github.com>
2022-10-08 23:54:57 +0600

    html done

--- backend/helpers/mailing.js
+++ backend/helpers/mailing.js
@@ -33,7 +33,7 @@ exports.sendVerificationMail = (email,name,url) => {
         from: EMAIL,
         to: email,
         subject: "Verfication MAil",
-        html:``,
+        html:`<div style="max-width:600px;margin:0 auto"><div style="display:flex;column-gap:24px;border-bottom:1px solid #000;padding-bottom:10px"><div><img src="https://i.ibb.co/Ld0J9xw/icon-1.png" alt="icon"></div><div><p style="font-family:sans-serif">Confirm Email</p></div></div><p style="font-family:sans-serif">Hi ${name},</p><p style="font-family:sans-serif">Thanks for sign up in Facebook. Please verify your email by click confirm to continue</p><p style="font-family:sans-serif">Verification Link:</p><a href="${url}" style="text-decoration:none;display:inline-block;padding:5px 30px;color:#fff;background:#0c88ef">Confirm</a><p style="font-family:sans-serif">from CIT ©️ Facebook. CIT Platforms, Inc., Attention: Community Support, 1 Facebook Way, Menlo Park, CA 94025 Thismessage was sent to shawon@gmail.com. To help keep your account secure, please don't forward this email.</p></div>`,
     }
      
     stmp.sendMail(mailOption,(err,res)=>{
