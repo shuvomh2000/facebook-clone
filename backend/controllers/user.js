e6097bee64502dabbb42c25e2a120c7aeae96b6a
Shuvo MH <96376185+shuvomh2000@users.noreply.github.com>
2022-10-30 16:37:55 +0600

    3pages add

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -91,4 +91,42 @@ exports.register = async (req, res) => {
 exports.activeAccount = async (req,res) => {
   const {token} = req.body
   const user = jwt.verify(token, process.env.TOKEN_SECRET)
+  const check = await User.findById(user.id)
+  if(check.varified == true){
+    return res.status(400).json({message:"this Account is already activated"})
+  }else{
+    await User.findByIdAndUpdate(user.id,{varified:true})
+    return res.status(200).json({message:"Account has been activated"})
+  }
+}
+
+exports.login =async (req,res) =>{
+    try {
+      const {email,password} = req.body
+      const user = await User.findOne({email})
+      if(!user){
+        return res.status(400).json({message:"this email is connected to your account"})
+      }
+      const check = await bcrypt.compare(password,user.password)
+      if(!check){
+        return res.status(400).json({message:"invalid password, try again"})
+      }
+
+      const token = generateToken({id: user._id},'7d')
+      res.send({
+        id:user._id,
+        username: user.username,
+        first_name:user.first_name,
+        last_name:user.last_name,
+        token: token,
+        varified:user.varified,
+        message:"Login Succesfull"
+        // Register Success active your email
+      });
+
+      
+
+    } catch (error) {
+      res.status(500).json({message:"error.message"})
+    }
 }
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { register, activeAccount } = require("../controllers/user");
+const { register, activeAccount, login } = require("../controllers/user");
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/activate", activeAccount);
+router.post("/login", login);
 
 module.exports = router;
