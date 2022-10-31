29b6930f131c7be6e83f9363766a41f5a9262841
Shuvo MH <96376185+shuvomh2000@users.noreply.github.com>
2022-10-31 23:48:33 +0600

    login box design done

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -1,6 +1,40 @@
 import React from "react";
 import "./login.css";
-import { Container, Grid,TextField } from "@mui/material";
+import { Container, Grid,TextField,Button  } from "@mui/material";
+import { styled } from '@mui/material/styles';
+
+// for styled textfiled
+const CssTextField = styled(TextField)({
+    width:'100%',
+    marginBottom:'15px',
+    '& label.Mui-focused': {
+      color: '#166FE5',
+    },
+});
+
+// for login button styled
+const MainButton = styled(Button)(({ theme }) => ({
+ color:'#fff',
+ backgroundColor:'#166FE5',
+ width:'100%',
+ fontSize:20,
+ fontWeight:'500',
+ padding:'10px 15px',
+ textTransform: 'none',
+ marginBottom:'15px',
+ fontFamily: ['Poppins',' sans-serif'],
+}));
+
+const CreateButton = styled(Button)(({ theme }) => ({
+ color:'#fff',
+ backgroundColor:'#36A420',
+ padding:'10px 15px',
+ fontSize:20,
+ fontWeight:'500',
+ marginTop:'10px',
+ textTransform: 'capitalize',
+ fontFamily: ['Poppins',' sans-serif'],
+}));
 
 const Login = () => {
   return (
@@ -8,18 +42,29 @@ const Login = () => {
       <Container fixed>
         <Grid container spacing={4}>
           <Grid item xs={6}>
-            <p>
+            <p className="logo_text">
               Facebook helps you connect and share with the people in your life.
             </p>
           </Grid>
           <Grid item xs={6}>
-            <div className="box">
-              <TextField
-                id="outlined-basic"
-                label="Outlined"
-                variant="outlined"
-              />
-            </div>
+              <div className="box">
+                <CssTextField
+                  id="outlined-basic"
+                  label="Email adress"
+                  variant="outlined"
+                />
+                <CssTextField
+                  id="outlined-basic"
+                  label="Password"
+                  variant="outlined"
+                  type="password"
+                />
+                <MainButton variant="contained">Log in</MainButton>
+                <p className="forget">Forgotten password?</p>
+                <div style={{textAlign:'center'}}>
+                    <CreateButton variant="contained">create new account</CreateButton>
+                </div>
+              </div>
           </Grid>
         </Grid>
       </Container>
