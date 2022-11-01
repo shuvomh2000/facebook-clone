f6ac42ef768b96ada29553142ed3297a62fd27e0
Shuvo MH <96376185+shuvomh2000@users.noreply.github.com>
2022-11-01 22:28:21 +0600

    login page done

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -1,39 +1,43 @@
 import React from "react";
 import "./login.css";
-import { Container, Grid,TextField,Button  } from "@mui/material";
-import { styled } from '@mui/material/styles';
+import { Container, Grid, TextField, Button } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { Link } from "react-router-dom";
 
 // for styled textfiled
 const CssTextField = styled(TextField)({
-    width:'100%',
-    marginBottom:'15px',
-    '& label.Mui-focused': {
-      color: '#166FE5',
-    },
+  width: "100%",
+  marginBottom: "15px",
+  "& label.Mui-focused": {
+    color: "#166FE5",
+  },
 });
 
 // for login button styled
 const MainButton = styled(Button)(({ theme }) => ({
- color:'#fff',
- backgroundColor:'#166FE5',
- width:'100%',
- fontSize:20,
- fontWeight:'500',
- padding:'10px 15px',
- textTransform: 'none',
- marginBottom:'15px',
- fontFamily: ['Poppins',' sans-serif'],
+  color: "#fff",
+  backgroundColor: "#166FE5",
+  width: "100%",
+  fontSize: 20,
+  fontWeight: "500",
+  padding: "10px 15px",
+  textTransform: "none",
+  marginBottom: "15px",
+  fontFamily: ["Poppins", " sans-serif"],
 }));
 
 const CreateButton = styled(Button)(({ theme }) => ({
- color:'#fff',
- backgroundColor:'#36A420',
- padding:'10px 15px',
- fontSize:20,
- fontWeight:'500',
- marginTop:'10px',
- textTransform: 'capitalize',
- fontFamily: ['Poppins',' sans-serif'],
+  color: "#fff",
+  backgroundColor: "#36A420",
+  padding: "10px 15px",
+  fontSize: 20,
+  fontWeight: "500",
+  marginTop: "10px",
+  textTransform: "capitalize",
+  fontFamily: ["Poppins", " sans-serif"],
+  "&:hover":{
+    backgroundColor: '#38c41c'
+  }
 }));
 
 const Login = () => {
@@ -42,29 +46,35 @@ const Login = () => {
       <Container fixed>
         <Grid container spacing={4}>
           <Grid item xs={6}>
+            <img src="icons/facebook.svg"/>
             <p className="logo_text">
               Facebook helps you connect and share with the people in your life.
             </p>
           </Grid>
           <Grid item xs={6}>
-              <div className="box">
-                <CssTextField
-                  id="outlined-basic"
-                  label="Email adress"
-                  variant="outlined"
-                />
-                <CssTextField
-                  id="outlined-basic"
-                  label="Password"
-                  variant="outlined"
-                  type="password"
-                />
-                <MainButton variant="contained">Log in</MainButton>
-                <p className="forget">Forgotten password?</p>
-                <div style={{textAlign:'center'}}>
-                    <CreateButton variant="contained">create new account</CreateButton>
-                </div>
+            <div className="box">
+              <CssTextField
+                id="outlined-basic"
+                label="Email adress"
+                variant="outlined"
+              />
+              <CssTextField
+                id="outlined-basic"
+                label="Password"
+                variant="outlined"
+                type="password"
+              />
+              <MainButton variant="contained">Log in</MainButton>
+              <Link to="/" className="forget">
+                Forgotten password?
+              </Link>
+
+              <div style={{ textAlign: "center" }}>
+                <CreateButton variant="contained">
+                  create new account
+                </CreateButton>
               </div>
+            </div>
           </Grid>
         </Grid>
       </Container>
